e16eb25eb4e9250fbc2cb7cc92dc2239f7892bd2
MrAnacin <62877260+MrAnacin@users.noreply.github.com>
2023-07-08 23:55:22 +0500

    -

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render(){
+    return(
+      <div>
+        <div className="login_block">
+          <input type="text" />
+          <input type="password" />
+          <input type="submit" />
+        </div>
+      </div>
+    )
+  }
+}
