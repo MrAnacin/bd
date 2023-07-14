6985dcfa422af45366c0f1bc62c55a20c6bf5c43
MrAnacin <62877260+MrAnacin@users.noreply.github.com>
2023-07-14 13:45:25 +0500

    -

--- src/components/registartion/reg.jsx
+++ src/components/registartion/reg.jsx
@@ -1,58 +1,58 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import styles from '../registartion/reg.module.css'
+import logo_log from '../../image/logo_log.png'
 
-const PasswordForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
+const RegistrationPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatchError, setPasswordMatchError] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (password === confirmPassword) {
-      // Пароли совпадают, выполняем дополнительные действия здесь
-    } else {
-      setPasswordsMatch(false);
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordMatchError(true);
+      return;
     }
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
+
+    console.log('Регистрация выполнена');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            placeholder="Логин"
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleInputChange}
-          />
+    <div className={styles.reg_box}>
+      <div>
+        <img className={styles.logo_log_box} src={logo_log} alt="logo" />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.email_reg_box}>
+          <input className={styles.reg_email} placeholder='Логин' type="email" value={email} onChange={handleEmailChange} required />
         </div>
-      <label>
-        Пароль:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Повторите пароль:
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
-      </label>
-      <br />
-      {!passwordsMatch && <p>Пароли не совпадают</p>}
-      <button type="submit">Отправить</button>
-    </form>
+        <div className={styles.password_reg_box}>
+          <input className={styles.reg_password} placeholder='Пароль' type="password" value={password} onChange={handlePasswordChange} required />
+        </div>
+        <div className={styles.confirmpassword_reg_box}>
+          <input className={styles.reg_confirm_password} placeholder='Подтвердите пароль' type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
+        </div>
+        {passwordMatchError && <p style={{ color: 'red' }}>Пароли не совпадают</p>}
+        <div className={styles.btn_reg_box}>
+          <button className={styles.btn_reg} type="submit">Зарегистрироваться</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default PasswordForm;
+export default RegistrationPage;
